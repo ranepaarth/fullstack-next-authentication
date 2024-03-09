88b4e6fea974247179ea13455791a203e1c93970
ranepaarth <2019paarth.rane@ves.ac.in>
2024-03-09 15:51:01 +0530

    bug: no response status

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -3,19 +3,28 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   try {
     if (!req.cookies.get(process.env.COOKIE_NAME!)) {
-      return NextResponse.json({ success: true, json: "Unauthorized access" });
+      return NextResponse.json(
+        { success: true, json: "Unauthorized access" },
+        { status: 200 }
+      );
     }
-    const response = NextResponse.json({ success: true, message: "logout" });
+    const response = NextResponse.json(
+      { success: true, message: "logout" },
+      { status: 200 }
+    );
 
     response.cookies.set(process.env.COOKIE_NAME!, "", {
       httpOnly: true,
-      secure:true,
+      secure: true,
       expires: new Date(0),
     });
 
     return response;
   } catch (error: any) {
     console.log(error);
-    return NextResponse.json({ success: false, message: error?.message });
+    return NextResponse.json(
+      { success: false, message: error?.message },
+      { status: 400 }
+    );
   }
 }
